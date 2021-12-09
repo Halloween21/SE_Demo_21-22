47d387eca61a7a9f88a78c3cd8537940d3db4a54
Halloween21 <simon@alacarte-kurier.de>
2021-12-09 13:23:07 +0100

    solved hw9

--- HW9/src/test/TestCalculator.java
+++ HW9/src/test/TestCalculator.java
@@ -1,5 +1,8 @@
 package test;
 
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -13,32 +16,32 @@ class TestCalculator {
 
 	@Test
 	public void Should_add_two_numbers_and_return_result() {
-		// Implement
+	    assertEquals(2, calculator.add(1, 1));
 	}
 
 	@ParameterizedTest
-	@CsvSource({})
+	@CsvSource(value = {"2,3" , "1,4"})
 	public void Should_add_two_numbers_and_return_result_parameterized(int valueOne, int valueTwo) {
-		// Implement
+		 assertEquals(5, calculator.add(valueOne, valueTwo));
 	}
 	
 	@Test
 	public void Should_substract_two_numbers_and_return_result() {
-		// Implement
+		 assertEquals(2, calculator.sub(5, 3));
 	}
 	
 	@Test
 	public void Should_multiply_two_numbers_and_return_result() {
-		// Implement
+		 assertEquals(6, calculator.multiply(2, 3));
 	}
 	
 	@Test
 	public void Should_divide_two_numbers_and_return_result() {
-		// Implement
+		 assertEquals(1, calculator.divide(1, 1));
 	}
 	
 	@Test
 	public void Should_throw_an_arithmetic_exception_if_denominator_is_zero() {
-		// Implement
+		Throwable exception = assertThrows(ArithmeticException.class, () -> calculator.divide(0, 0) );
 	}
 }
--- HW9/src/test/TestCalendar.java
+++ HW9/src/test/TestCalendar.java
@@ -1,27 +1,42 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.GregorianCalendar;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import main.Calendar;
 
 class TestCalendar {
 
 	@ParameterizedTest
-	@ValueSource()
+	@ValueSource(ints = { 4, 40, 80, 2000, 2004 })
 	public void Should_return_true(int year) {
-		// Implement
+		Calendar c = new Calendar(year);
+		assertTrue(c.isLeapYear());
 	}
 
 	@ParameterizedTest
-	@ValueSource()
+	@ValueSource(ints = { 3, 45, 346, 2001, 2345 })
 	public void Should_return_false(int year) {
-		// Implement
+		Calendar c = new Calendar(year);
+		assertFalse(c.isLeapYear());
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = { 4, 40, 80, 2000, 2004 })
+	public void Should_return_if_year_is_leap(int year) {
+		GregorianCalendar gc = new GregorianCalendar();
+		assertTrue(gc.isLeapYear(year));
 	}
 
 	@ParameterizedTest
 	@ValueSource()
-	public void Should_return_if_year_is_leap(int year) {
-		// Implement
+	public void boundary_test(int lowest, int highest) {
+		
 	}
-
-	// Create a new method for boundary testing
 }
